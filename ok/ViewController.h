5b7ed85cbeaff8312dc959f3cea2af6a893438b7
superCode <iygnaw361@yeah.net>
2016-07-12 03:43:08 +0800

    Initial Commit

---
+++ ok/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ok
+//
+//  Created by xixing1 on 16/7/12.
+//  Copyright © 2016年 superCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ok/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ok
+//
+//  Created by xixing1 on 16/7/12.
+//  Copyright © 2016年 superCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
